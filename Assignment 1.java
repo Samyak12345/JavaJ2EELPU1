bfdcbd4383e2a1e3a238cfb64ace8ff699d7274a
Samyak Jain <30800775+Samyak12345@users.noreply.github.com>
2020-08-18 11:36:45 +0530

    Add files via upload

---
+++ Assignment 1.java
@@ -0,0 +1,14 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.print("\u0938");
+		System.out.print("\u092E");
+		System.out.print("\u092F");
+		System.out.print("\u0915");
+		System.out.print("  ");
+		System.out.print("\u091C");
+		System.out.print("\u0948");
+		System.out.print("\u0928");
+	}
+}
